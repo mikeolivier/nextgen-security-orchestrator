db9b56c851a4c4bf0905340760c4c2ac6f2cfb3d
mike <oliviermike3@gmail.com>
2026-08-16 23:05:38 -0400

    Refactor: introduce orchestrator module structure

---
+++ orchestrator/lambda.py
@@ -0,0 +1,11 @@
+
+
+from incident import Incident
+from notification_engine import notify
+
+
+def lambda_handler(event, context):
+
+    print("Security Orchestrator Started")
+
+    # Existing logic will be migrated here
