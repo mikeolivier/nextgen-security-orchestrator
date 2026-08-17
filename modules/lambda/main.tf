67e233ff590b350169164432e79c2b1165f7225a
mike <oliviermike3@gmail.com>
2026-08-17 00:55:21 -0400

    Refactor Lambda packaging using archive provider

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -52,8 +52,8 @@ resource "aws_lambda_function" "iam_monitor" {
   handler = "lambda.lambda_handler"
   role    = aws_iam_role.lambda_role.arn
 
-  filename         = "${path.module}/lambda.zip"
-  source_code_hash = filebase64sha256("${path.module}/lambda.zip")
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 
   environment {
     variables = {
